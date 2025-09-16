f922298fd7a2508eda0d89cd47cb5ca7bc94ebb0
Hans Queja <hansqueja8@gmail.com>
2025-09-16 22:18:17 +0800

    Add: preliminary POST API

--- src/api/endpoints.py
+++ src/api/endpoints.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Form, UploadFile
+from fastapi.responses import JSONResponse, FileResponse
+
 
 app = FastAPI()
 
@@ -6,3 +9,15 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/upload/")
+def get_lyrics(
+    audio: UploadFile,
+    lyrics: Annotated[str, Form()],
+):
+    
+    if audio.content_type not in ["audio/mpeg", "audio/wav"]:
+        return JSONResponse(content={"error": "Invalid file type. Only MP3 and WAV are supported."}, status_code=400)
+
+    return JSONResponse(content={"message": "File Received"}, status_code=200)
